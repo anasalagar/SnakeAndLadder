79f7c766710cffd366f24702cb45a8c4caa583bd
anasalagar <90851784+anasalagar@users.noreply.github.com>
2021-10-13 22:27:05 +0530

    added UC_1

---
+++ UC_1.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnekeAndLadder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the snake and ladder game");
+            Console.WriteLine();
+            Console.WriteLine("Player mode:single: Starting position at 0");
+            Console.WriteLine("*Start Game*");
+            int position = 0;
+            int dice;
+            Console.ReadLine();
+
+        }
+    }
+}
